a91a4182b7a4a965b780fee6acea8d74156f779c
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2026-05-20 17:19:01 -0700

    Set up serverless environment config for connecting to database

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,17 +1,10 @@
 'use server';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get latest products
 export async function getLatestProducts() {
-  const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-  });
-
-  const prisma = new PrismaClient({ adapter });
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
