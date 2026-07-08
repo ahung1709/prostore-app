9268b914e2164ac56f4a217b4bd6a9712e527a02
ahung1709 <ahungwk@gmail.com>
2026-07-08 00:29:01 -0700

    Add remove from cart function and dynamic cart button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,12 +1,12 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { Plus } from 'lucide-react';
-import { CartItem } from '@/types';
+import { Plus, Minus } from 'lucide-react';
+import { Cart, CartItem } from '@/types';
 import { toast } from 'sonner';
-import { addItemToCart } from '@/lib/actions/cart.actions';
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
   const handleAddToCart = async () => {
@@ -32,7 +32,39 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     );
   };
 
-  return (
+  // Handle remove from cart
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    toast(
+      <div className='flex items-center justify-between gap-2'>
+        <span className='text-sm'>{res.message}</span>
+      </div>,
+    );
+
+    return;
+  };
+
+  // Check if item is in cart
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+        <Minus className='h-4 w-4' />
+      </Button>
+      <span className='px-2'>{existItem.qty}</span>
+      <Button type='button' variant='outline' onClick={handleAddToCart}>
+        <Plus className='h-4 w-4' />
+      </Button>
+    </div>
+  ) : (
     <Button
       className='w-full cursor-pointer'
       type='button'
